0d229a6d15cb20d8856af9ad79a947d3c515b63e
mkropidlowski <mkropidlowsky@gmail.com>
2023-09-14 14:46:44 +0200

    feat: add all-attraction page

---
+++ app/(allAttraction)/all-attraction/page.tsx
@@ -0,0 +1,31 @@
+import Attraction from "@/app/components/popularPlacesSection/components/atraction";
+import getDataFromCollection from "@/app/helpers/getDataFromCollection";
+import { BE_Attraction } from "@/types/types";
+
+const AttractionPage = async () => {
+    const attraction = await getDataFromCollection("attraction", 3);
+    return (
+        <div className="flex md:flex-row flex-col justify-center items-center m-auto mt-[135px] gap-[45px]">
+            {attraction.documents.map((attraction: BE_Attraction) => (
+                <Attraction
+                    key={attraction?._id}
+                    _id={attraction?._id}
+                    name={attraction?.name}
+                    location={attraction?.location}
+                    pricePerDay={attraction?.pricePerDay}
+                    image={attraction?.image}
+                    currency={attraction?.currency}
+                    allAmenities={{
+                        propertyAmenities: undefined,
+                        roomAmenities: undefined,
+                        wellnessSpa: undefined,
+                        accessibility: undefined,
+                        forChildren: undefined,
+                    }}
+                />
+            ))}
+        </div>
+    );
+};
+
+export default AttractionPage;
--- app/components/popularPlacesSection/popularPlaces.tsx
+++ app/components/popularPlacesSection/popularPlaces.tsx
@@ -1,29 +1,40 @@
 import getDataFromCollection from "@/app/helpers/getDataFromCollection";
 import Attraction from "./components/atraction";
 import { BE_Attraction } from "@/types/types";
+import Button from "../button/button";
+import Link from "next/link";
 
 const PopularPlaces = async () => {
     const attraction = await getDataFromCollection("attraction", 3);
     return (
-        <div className="flex md:flex-row flex-col justify-center items-center m-auto mt-[35px] gap-[45px]">
-            {attraction.documents.map((attraction: BE_Attraction) => (
-                <Attraction
-                    key={attraction?._id}
-                    _id={attraction?._id}
-                    name={attraction?.name}
-                    location={attraction?.location}
-                    pricePerDay={attraction?.pricePerDay}
-                    image={attraction?.image}
-                    currency={attraction?.currency}
-                    allAmenities={{
-                        propertyAmenities: undefined,
-                        roomAmenities: undefined,
-                        wellnessSpa: undefined,
-                        accessibility: undefined,
-                        forChildren: undefined,
-                    }}
-                />
-            ))}
+        <div className="w-full flex flex-col gap-8 justify-center items-center">
+            <div className="flex md:flex-row flex-col justify-center items-center m-auto mt-[35px] gap-[45px]">
+                {attraction.documents.map((attraction: BE_Attraction) => (
+                    <Attraction
+                        key={attraction?._id}
+                        _id={attraction?._id}
+                        name={attraction?.name}
+                        location={attraction?.location}
+                        pricePerDay={attraction?.pricePerDay}
+                        image={attraction?.image}
+                        currency={attraction?.currency}
+                        allAmenities={{
+                            propertyAmenities: undefined,
+                            roomAmenities: undefined,
+                            wellnessSpa: undefined,
+                            accessibility: undefined,
+                            forChildren: undefined,
+                        }}
+                    />
+                ))}
+            </div>
+            <div>
+                <Link href={"/all-attraction"}>
+                    <Button type="button" secondary className="bg-orange-600 text-xl hover:bg-orange-700">
+                        <span className="font-normal">Explore more...</span>
+                    </Button>
+                </Link>
+            </div>
         </div>
     );
 };
