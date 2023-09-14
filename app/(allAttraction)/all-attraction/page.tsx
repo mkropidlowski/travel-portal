4d4b3627565db3a3bd4744f8bf8f0e168c151293
mkropidlowski <mkropidlowsky@gmail.com>
2023-09-14 14:48:08 +0200

    edit: change css for all attraction

--- app/(allAttraction)/all-attraction/page.tsx
+++ app/(allAttraction)/all-attraction/page.tsx
@@ -3,9 +3,9 @@ import getDataFromCollection from "@/app/helpers/getDataFromCollection";
 import { BE_Attraction } from "@/types/types";
 
 const AttractionPage = async () => {
-    const attraction = await getDataFromCollection("attraction", 3);
+    const attraction = await getDataFromCollection("attraction");
     return (
-        <div className="flex md:flex-row flex-col justify-center items-center m-auto mt-[135px] gap-[45px]">
+        <div className="max-w-[1240px] flex md:flex-row flex-col flex-wrap justify-center items-center m-auto mt-[135px] gap-[45px]">
             {attraction.documents.map((attraction: BE_Attraction) => (
                 <Attraction
                     key={attraction?._id}
