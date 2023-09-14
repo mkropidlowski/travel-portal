e6e214c1f2e6e5ab7c44ab909d6fe24d2171bcd1
mkropidlowski <mkropidlowsky@gmail.com>
2023-09-14 15:01:48 +0200

    feat: create allPosts page

---
+++ app/(allPosts)/all-posts/page.tsx
@@ -0,0 +1,16 @@
+import Post from "@/app/components/blog/components/post";
+import { PostProps } from "@/app/components/blog/types";
+import getDataFromCollection from "@/app/helpers/getDataFromCollection";
+
+const AllPostPage = async () => {
+    const posts = await getDataFromCollection("blog");
+    return (
+        <div className="max-w-[1240px] flex md:flex-row flex-col flex-wrap justify-center items-center m-auto mt-[135px] gap-[45px]">
+            {posts.documents.map((post: PostProps) => (
+                <Post key={post._id} _id={post._id} title={post.title} date={post.date} image={post.image} />
+            ))}
+        </div>
+    );
+};
+
+export default AllPostPage;
--- app/components/blog/blog.tsx
+++ app/components/blog/blog.tsx
@@ -1,14 +1,25 @@
+import Link from "next/link";
+import Button from "../button/button";
 import Post from "./components/post";
 import { PostProps } from "./types";
 import getDataFromCollection from "@/app/helpers/getDataFromCollection";
 
 const Blog = async () => {
-    const posts = await getDataFromCollection("blog");
+    const posts = await getDataFromCollection("blog", 4);
     return (
-        <div className="h-full flex flex-col md:flex-row flex-wrap justify-center items-center m-auto mt-[35px] gap-[45px]">
-            {posts.documents.map((post: PostProps) => (
-                <Post key={post._id} _id={post._id} title={post.title} date={post.date} image={post.image} />
-            ))}
+        <div className="w-full flex flex-col gap-7 justify-center items-center">
+            <div className="h-full flex flex-col md:flex-row flex-wrap justify-center items-center m-auto mt-[35px] gap-[45px]">
+                {posts.documents.map((post: PostProps) => (
+                    <Post key={post._id} _id={post._id} title={post.title} date={post.date} image={post.image} />
+                ))}
+            </div>
+            <div>
+                <Link href={"/all-posts"}>
+                    <Button type="button" secondary className="bg-orange-600 text-xl hover:bg-orange-700">
+                        <span className="font-normal">Explore more...</span>
+                    </Button>
+                </Link>
+            </div>
         </div>
     );
 };
