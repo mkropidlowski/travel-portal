9c207f51211b613bbc2d0ed0766bdc20131f1173
mkropidlowski <mkropidlowsky@gmail.com>
2023-09-26 17:13:45 +0200

    feat: change login form styles

--- app/(auth)/login/layout.tsx
+++ app/(auth)/login/layout.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/app/components/navbar/navbar";
 
-export default async function BlogLayout({ children }: { children: React.ReactNode }) {
+export default async function AuthLayout({ children }: { children: React.ReactNode }) {
     return (
         <>
             <div className="max-w-[1140px] max-h-max flex items-center flex-col m-auto">{children}</div>
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,8 +2,8 @@ import AuthForm from "./components/AuthForm";
 
 const LoginPage = () => {
     return (
-        <div className="flex justify-center items-center flex-col w-[900px] h-screen">
-            <h2 className="text-[30px] font-medium">Hello, login or create account.</h2>
+        <div className="flex justify-center items-center flex-col max-w-[900px] h-screen gap-7">
+            <h2 className="text-[30px] font-medium max-w-[300px] text-center">Hello, login or create account.</h2>
             <AuthForm />
         </div>
     );
