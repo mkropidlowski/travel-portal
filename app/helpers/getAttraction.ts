4ec54a098588773e0a5a6b4616094648850aab82
mkropidlowski <mkropidlowsky@gmail.com>
2023-10-17 13:54:22 +0200

    change api url

--- app/helpers/getAttraction.ts
+++ app/helpers/getAttraction.ts
@@ -1,9 +1,13 @@
 import { BE_Attraction } from "@/types/types";
 
-const API = process.env.NEXT_API_LOCAL_URL;
+const API = process.env.NEXT_API_VERCEL_URL;
 
 export const getAttraction = async (): Promise<BE_Attraction> => {
-    const res = await fetch(`http://localhost:3000/api/attraction`, {
+    const isLocalhost = Boolean(window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1");
+
+    const baseUrl = isLocalhost ? "http://localhost:3000" : API;
+
+    const res = await fetch(`${baseUrl}/api/attraction`, {
         cache: "no-store",
     });
     if (!res.ok) {
