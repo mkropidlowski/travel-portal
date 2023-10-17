5713574954dc5655a0322ca4c5870711e6cc35b4
mkropidlowski <mkropidlowsky@gmail.com>
2023-10-17 13:58:07 +0200

    edit

--- app/helpers/getAttraction.ts
+++ app/helpers/getAttraction.ts
@@ -1,11 +1,9 @@
 import { BE_Attraction } from "@/types/types";
 
-const API = process.env.NEXT_API_VERCEL_URL;
-
 export const getAttraction = async (): Promise<BE_Attraction> => {
     const isLocalhost = Boolean(window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1");
 
-    const baseUrl = isLocalhost ? "http://localhost:3000" : API;
+    const baseUrl = isLocalhost ? "http://localhost:3000" : "https://travel-portal-five.vercel.app";
 
     const res = await fetch(`${baseUrl}/api/attraction`, {
         cache: "no-store",
